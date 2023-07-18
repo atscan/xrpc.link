bbbbbd1d51a30c65ee2c3ddf3b5220daae730922
tree <tree@hidden.cz>
2023-07-19 01:12:38 +0200

    update

--- service.js
+++ service.js
@@ -1,53 +1,87 @@
-import { serve } from "https://deno.land/std@0.194.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
+class XError extends Error {}
 const Config = {
   atscan: "https://api.atscan.net",
   defaultPds: "https://bsky.social",
-  redirs: {
-    "(r|repo|getRepo)(/([^\/]+)|)": "/xrpc/com.atproto.sync.getRepo?did=@did",
-    "(i|ds|describe|describesServer)":
-      "/xrpc/com.atproto.server.describeServer",
-    //"(d|did)": "https://api.atscan.net/%1",
-  },
 };
 
-serve(async (r) => {
-  const src = new URL(r.url);
-  let route, match;
-  for (const k in Config.redirs) {
-    const re = new RegExp(k, "i");
-    match = src.pathname?.trim().substring(1).match(re);
-    if (match) {
-      route = Config.redirs[k];
-      break;
-    }
-  }
-  if (!match) {
-    return new Response(null, { status: 404 });
-  }
-  let did;
-  if (route.indexOf("@did") !== -1) {
-    did = await resolveDid(match[3]);
-    if (!did) {
-      return new Response(null, { status: 404 });
-    }
-    route = route.replace("@did", did.did);
-  }
-  match.forEach((u, i) => (route = route.replace("%" + i, u || "")));
-  const Location = [
-    route.indexOf("http") != -1 ? "" : (did?.pds[0] || Config.defaultPds),
-    route.substring(1),
-  ].filter((x) => x).join(
-    "/",
-  );
-  return new Response("", {
-    status: 302,
-    headers: { Location },
+const router = new Router();
+router
+  // ----------------------------
+  // START OF ROUTES
+  // ----------------------------
+  // route /
+  // ----------------------------
+  .get("/", (ctx) => {
+    ctx.response.redirect("https://atscan.net");
+  })
+  // ----------------------------
+  // route /ds/:did - Describe Server
+  // ----------------------------
+  .get("/ds/:pds?", async (ctx) => {
+    const pds = ctx.params.pds || Config.defaultPds;
+    return ctx.response.redirect(
+      `https://${
+        pds.replace(/^https?:\/\//, "")
+      }/xrpc/com.atproto.server.describeServer`,
+    );
+  })
+  // ----------------------------
+  // route /r/:did - (Get) Repo
+  // ----------------------------
+  .get("/r/:did", async (ctx) => {
+    const { did, pds } = await params(ctx);
+    return ctx.response.redirect(
+      `${pds}/xrpc/com.atproto.sync.getRepo?did=${did}`,
+    );
+  })
+  // ----------------------------
+  // route /c/:did - Checkout
+  // ----------------------------
+  .get("/c/:did", async (ctx) => {
+    const { did, pds } = await params(ctx);
+    return ctx.response.redirect(
+      `${pds}/xrpc/com.atproto.sync.getCheckout?did=${did}`,
+    );
   });
-});
+// ----------------------------
+// END OF ROUTES
+// ----------------------------
 
-async function resolveDid(did) {
-  return fetch([Config.atscan, did].join("/")).then((r) => {
+async function params(ctx) {
+  let did = ctx.params.did;
+  let pds = Config.defaultPds;
+  if (!did.match(/^did:plc:/)) {
+    const fd = await didInfo(did);
+    if (!fd) {
+      ctx.response.status = 404;
+      throw new XError("DID not found");
+    }
+    did = fd.did;
+    pds = fd.pds;
+  }
+  return { did, pds };
+}
+
+async function didInfo(str) {
+  return fetch([Config.atscan, str].join("/")).then((r) => {
     return r.status === 200 ? r.json() : null;
   });
 }
+
+const app = new Application();
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    console.error(`[${ctx.url}]`);
+    console.error(err);
+    ctx.response.body = `${err}`;
+  }
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Listening at :8764`);
+await app.listen({ port: 8764 });
